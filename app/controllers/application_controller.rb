5800004fdce09c38e67ee1c386e6dfb36a4fba68
ben <bjelinek42@gmail.com>
2022-08-10 14:31:52 -0600

    tournament create action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,4 @@
 class ApplicationController < ActionController::API
 
-  def index
-    tournaments = Tournament.all
-    render json: t
-  end
 
-  def show
-    tournament = Tournament.find_by(id: params[:id])
-    render json: tournament
-  end
-  
 end
--- app/controllers/tournaments_controller.rb
+++ app/controllers/tournaments_controller.rb
@@ -1,2 +1,22 @@
 class TournamentsController < ApplicationController
+  def index
+    tournaments = Tournament.all
+    render json: t
+  end
+
+  def show
+    tournament = Tournament.find_by(id: params[:id])
+    render json: tournament
+  end
+
+  def create
+    tournament = Tournament.new(
+      name: params[:name],
+      location: params[:location],
+      date: params[:date],
+      team_count: params[:team_count]
+    )
+    tournament.save
+    render json: tournament
+  end
 end
