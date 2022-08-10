5c1d001aa4a44b5c6911473796097eb45bbbaa3e
ben <bjelinek42@gmail.com>
2022-08-10 14:07:32 -0600

    tournament index and show action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::API
+
+  def index
+    tournaments = Tournament.all
+    render json: t
+  end
+
+  def show
+    tournament = Tournament.find_by(id: params[:id])
+    render json: tournament
+  end
+  
 end
