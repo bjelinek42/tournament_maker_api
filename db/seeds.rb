0657874824502fb2aa180e4a5480d2b70e787929
ben <bjelinek42@gmail.com>
2022-08-11 10:36:58 -0600

    teams seed and controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,47 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-t = Tournament.new(name: "Colorado Cup", location: "Aurora, CO", team_count: 8, date: "2022-06-18")
-t.save
+# t = Tournament.new(name: "Colorado Cup", location: "Aurora, CO", team_count: 8, date: "2022-06-18")
+# t.save
 
-t = Tournament.new(name: "Mountain Getdown", location: "Aspen, CO", team_count: 16, date: "2022-07-20")
-t.save
+# t = Tournament.new(name: "Mountain Getdown", location: "Aspen, CO", team_count: 16, date: "2022-07-20")
+# t.save
 
-t = Tournament.new(name: "Spring Cleaning", location: "Colorado Springs, CO", team_count: 7, date: "2022-04-10")
-t.save
+# t = Tournament.new(name: "Spring Cleaning", location: "Colorado Springs, CO", team_count: 7, date: "2022-04-10")
+# t.save
 
-t = Tournament.new(name: "Winter Warm-up", location: "Fort Collins, CO", team_count: 24, date: "2022-12-5")
-t.save
+# t = Tournament.new(name: "Winter Warm-up", location: "Fort Collins, CO", team_count: 24, date: "2022-12-5")
+# t.save
+
+# team = Team.new(name: "Fungi", location: "Boulder, CO", logo: "https://cdn.ultiworld.com/wordpress/wp-content/uploads/2021/08/golden-fungi-logo.png")
+# team.save
+
+# team = Team.new(name: "Syndicate", location: "Colorado Springs, CO", logo: "https://pbs.twimg.com/profile_images/761619475611299840/UsUn08L2_400x400.jpg")
+# team.save
+
+# team = Team.new(name: "Jonny Bravo", location: "Denver, CO", logo: "https://cdn.ultiworld.com/wordpress/wp-content/uploads/2012/07/Bravo-300x249.jpg")
+# team.save
+
+# team = Team.new(name: "Inception", location: "Denver, CO", logo: "https://cdn.ultiworld.com/wordpress/wp-content/uploads/2015/08/inception-logo.jpg")
+# team.save
+
+# team = Team.new(name: "Jonny Encore", location: "Denver, CO", logo: "https://www.ultiphotos.com/img/s/v-10/p3539270322-3.jpg")
+# team.save
+
+# team = Team.new(name: "IsoAtmo", location: "Denver, CO", logo: "https://pbs.twimg.com/profile_images/1264949590584786950/KMt6iGUq_400x400.jpg")
+# team.save
+
+# team = Team.new(name: "Demo", location: "Des Moines, IA", logo: "https://d36m266ykvepgv.cloudfront.net/uploads/media/nUf4kiMvXn/c-324-247-845-1-324-247/centraliowaultimateblue.png")
+# team.save
+
+# team = Team.new(name: "CryptIC", location: "Iowa City, IA", logo: "https://i0.wp.com/blog.nscsports.org/wp-content/uploads/2019/09/USA-Ultimate-Regionals-at-NSC-9.22.19-1-8.jpg?ssl=1")
+# team.save
+
+# team = Team.new(name: "Grey Duck", location: "Minneapolis, MN", logo: "https://pbs.twimg.com/profile_images/1192643866618404865/GGb2H6Uw_400x400.jpg")
+# team.save
+
+# team = Team.new(name: "Hodags", location: "Madison, WI", logo: "https://collegechampionships.usaultimate.org/wp-content/uploads/2019/04/Wisconsin_M_2019.jpg")
+# team.save
+
+# team = Team.new(name: "Mamabird", location: "Boulder, CO", logo: "https://www.mamabird.com/img/Patch.png")
+# team.save
