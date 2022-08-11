15ff35946428e0d46237726eef93abc9ea516b5d
ben <bjelinek42@gmail.com>
2022-08-11 10:44:57 -0600

    teams create action

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -8,4 +8,18 @@ class TeamsController < ApplicationController
     team = Team.find_by(id: params[:id])
     render json: team
   end
+
+  def create
+    team = Team.new(
+      name: params[:name],
+      location: params[:location],
+      logo: params[:logo]
+    )
+    if team.save
+      render json: team
+    else
+      render json: "Error, team not saved."
+    end
+  end
+  
 end
